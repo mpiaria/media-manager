9cf0b54645e95ac03d797cec4b9c0cdb85d4a6eb
Michael Iaria <8315738+mpiaria@users.noreply.github.com>
2021-07-16 14:02:46 -0500

    Initial commit

---
+++ src/test/java/dev/mostlycloudy/mediamanager/MediaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mostlycloudy.mediamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
